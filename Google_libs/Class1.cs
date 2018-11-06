e55523e8801d1eda18a49bd6b9f9f44414e2a022
Mercenary <Mercenary@noname.com>
2018-11-06 19:53:47 +0300

    Добавьте файлы проекта.

---
+++ Google_libs/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Google;
+
+namespace GoogleLibs
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TLGBOT/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using GetProxy;
+using Telegram;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace TLGBOT
+{
+    class Program
+    {
+    
+    static WebProxy wp = new WebProxy(GetGoodProxy.ParseProxies(), true);
+
+        private static readonly TelegramBotClient Bot =
+            new TelegramBotClient("746900142:AAHG_g8mTS0HhuBNwkWZBOxNHGxCaiEyIvc", wp);
+
+        static int _count = 0;
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Телеграм-бот \"SHARP_POLICE\"");
+            Console.WriteLine("Адресс прокси: " + GetGoodProxy.ParseProxies());
+            try
+            {
+                Bot.OnMessage += Bot_OnMessage;
+                Bot.OnMessageEdited += Bot_OnMessage;
+
+                Bot.StartReceiving();
+                Console.ReadLine();
+                Bot.StopReceiving();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        private static void BOT_TEST(object sender, Telegram.Bot.Args.InlineQueryEventArgs w)
+        {
+            
+        }
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            
+            if (e.Message.Type == MessageType.TextMessage)
+            {
+                if (e.Message.Text != null)
+                {
+                    if (e.Message.Text == "/status")
+                    {
+                        Bot.SendTextMessageAsync(e.Message.Chat.Id, "Бот работает. Адрес прокси: "+ GetGoodProxy.ParseProxies());
+                    }
+                }
+                else
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Bad");
+                }
+            }
+        }
+    }
+}
